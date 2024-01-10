8572b006b6258c65b3e5f5350cf3334600310f3c
Dnyaneshwar <99679780+Dnyaneshwar01@users.noreply.github.com>
2024-01-10 08:12:43 +0530

    Test Java file Added
    JavaJenkinsTest.java

---
+++ JavaJenkinsTest.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class JavaJenkinsTest {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=8; i++) {
+			System.out.println("Run Eclipse program using cmd");
+			System.out.println(i);
+		}
+
+	}
+
+}
