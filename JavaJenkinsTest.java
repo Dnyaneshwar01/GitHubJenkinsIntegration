a98e3169f813cf2fae0606a712e75083310c1ba3
Dnyaneshwar <99679780+Dnyaneshwar01@users.noreply.github.com>
2024-10-20 21:01:56 +0530

    Update JavaJenkinsTest.java

--- JavaJenkinsTest.java
+++ JavaJenkinsTest.java
@@ -2,7 +2,7 @@ package JavaJenkinsTest;
 
 public class JavaJenkinsTest {
 
-	public static void main(String[] args) {
+	public static void main(String[]) {
 		for(int i=0; i<=8; i++) {
 			System.out.println("Run Eclipse program using cmd");
 			System.out.println(i);
